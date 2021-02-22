59d74a3df02d89c7f7e66e1516a930b72c2f18dc
Antoine J.C. Zink <77839973+Zink-Antoine@users.noreply.github.com>
2021-02-22 16:31:49 +0100

    Add files via upload

---
+++ R/Output.Summary.R
@@ -0,0 +1,57 @@
+#'Output.Summary
+#'
+#' final quantity presentation (output) MC GUM suppl1
+#'
+#' @param output output quantity
+#' @param p coverage probability
+#'
+#'
+#' @return a list object with the following elements
+#' @return $output.sort
+#' @return $esp.sort
+#' @return dev.sort
+#' @return cov.star
+#' @return low.cov
+#' @return high.cov
+#'
+#' @export
+#'
+#' @examples
+Output.Summary <-
+function (output,p){
+output.sort<-sort(output,decreasing = FALSE)
+# absolute growth test
+m<-length(output.sort)
+	while(length(m)>0){
+	output.diff<-output.sort-shift(output.sort,n=1)
+	m<-which(output.diff==0) #index where the deviation from the previous number is zero
+	output.sort[m]<-output.sort[m]+1e-128 #includes minimal disturbance
+}
+
+#coverage calculate
+M<-length(output)
+q<-p*M
+
+#symm?trique
+#r<-(M-q)/2
+#low<-r
+#high<-r+q
+#cov<-output.sort[high]-output.sort[low]
+
+#shortest
+cov.star<-M
+for (r in 1:(M-q)){
+low<-r
+high<-r+q
+cov<-output.sort[high]-output.sort[low]
+if(cov<cov.star) {
+cov.star<-cov
+low.star<-low
+high.star<-high
+}
+
+}
+
+return(list(output.sort=output.sort,esp.sort=mean(output.sort),dev.sort=sd(output.sort),cov.star=cov.star,low.cov=output.sort[low.star],high.cov=output.sort[high.star]))
+}
+
