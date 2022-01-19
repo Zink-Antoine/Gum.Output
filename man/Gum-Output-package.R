9e1aba23a366f4a2d6714914cdbdc94c63be3f46
Antoine J.C. Zink <77839973+Zink-Antoine@users.noreply.github.com>
2022-01-19 13:28:41 +0100

    Add files via upload

---
+++ man/Gum-Output-package.R
@@ -0,0 +1,10 @@
+#' Gum-Output
+#'
+#' final quantity presentation (output) MC GUM suppl1 and suppl2
+#'
+#' @docType package
+#' @name <GumOutput>
+#' @importFrom graphics contour filled.contour axis points
+#' @importFrom stats sd
+
+NULL
